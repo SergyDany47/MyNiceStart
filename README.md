49d5655c4789afa8ebbbb4cecfe8fab2c3b78f79
Sergio <sergiodani47@gmail.com>
2021-01-20 18:46:03 +0100

    Version FINAL

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 [SergyDany47](https://github.com/SergyDany47)
 > Android is love
 
-| splash              | login              | registration              | main              | main2              |    |
-|:--------------------|:-------------------|:--------------------------|:------------------|:-------------------|:---|
-| ![](img_app/splash.png) | ![](img_app/login.png) | ![](img_app/register.png) | ![](img_app/interfaz1.png) | ![](img_app/interfaz2.png) |    |
+| splash              | login              | registration              | main              | main2              |
+|:--------------------|:-------------------|:--------------------------|:------------------|:-------------------|
+| ![](img_app/splash.png) | ![](img_app/login.png) | ![](img_app/register.png) | ![](img_app/interfaz1.png) | ![](img_app/interfaz2.png)
--- app/src/main/java/com/example/mynicestart/MainActivity.java
+++ app/src/main/java/com/example/mynicestart/MainActivity.java
@@ -3,9 +3,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -32,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
                 .centerCrop()
                 .circleCrop()
                 .into(fondo);
+        ImageView mycontext = findViewById(R.id.img_guiri);
+        registerForContextMenu(mycontext);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.edit:
+                Toast toast = Toast.makeText(this, "going CONTEXT CAMERA", Toast.LENGTH_LONG);
+                toast.show();
+
+                break;
+            case R.id.delete:
+                Toast toast2 = Toast.makeText(this, "going CONTEXT SETTINGS", Toast.LENGTH_LONG);
+                toast2.show();
+
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 
 }
