d1504f7e89edf1767556a094c56a686ab2ec837c
Sergio <sergiodani47@gmail.com>
2021-03-10 16:29:58 +0100

    Version 1.0

--- app/src/main/java/com/example/mynicestart/MainActivity.java
+++ app/src/main/java/com/example/mynicestart/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.mynicestart;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -10,19 +15,26 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.button);
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+       /* Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
                         .this, MainActivity2.class);
                 startActivity(intent);
             }
-        });
+        });*/
+
+
         ImageView fondo = findViewById(R.id.img_guiri);
         Glide.with(this)
                 .load(R.drawable.img_guiri)
@@ -40,6 +54,43 @@ public class MainActivity extends AppCompatActivity {
         ImageView mycontext = findViewById(R.id.img_guiri);
         registerForContextMenu(mycontext);
     }
+
+    public void openMain2(View view) {
+        Intent intent = new Intent(MainActivity
+                .this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+
+        @Override
+
+        public void onRefresh() {
+
+            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.activity_main_context);
+
+            Snackbar snackbar = Snackbar
+
+                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+
+                    .setAction("UNDO", new View.OnClickListener() {
+
+                        @Override
+
+                        public void onClick(View view) {
+
+                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+        }
+
+    };
+
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
@@ -48,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_context, menu);
 
     }
+
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
--- app/src/main/java/com/example/mynicestart/MainActivity2.java
+++ app/src/main/java/com/example/mynicestart/MainActivity2.java
@@ -1,9 +1,16 @@
 package com.example.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.alespero.expandablecardview.ExpandableCardView;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,8 +18,47 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ExpandableCardView card = findViewById(R.id.profile);
+        card.setOnExpandedListener(new ExpandableCardView.OnExpandedListener() {
+            @Override
+            public void onExpandChanged(View v, boolean isExpanded) {
+                Toast.makeText(MainActivity2.this, isExpanded ? "Expanded!" :
+                        "Collapsed!", Toast.LENGTH_SHORT).show();
+                                               }
+                                           });
+
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_app_bar_menu_bookmark:
+                        Toast.makeText(MainActivity2.this, "Bookmark clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_search:
+                        Toast.makeText(MainActivity2.this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_share:
+                        Toast.makeText(MainActivity2.this, "Share clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity2.this, "Menu clicked!", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
     }
+
 
     public void openMain2(View view) {
     }
+
 }
